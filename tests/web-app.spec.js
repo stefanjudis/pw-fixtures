cea87e4a04e481d1c726137d2f259974a3a41dbb
stefan judis <stefanjudis@gmail.com>
2023-01-12 15:07:25 +0100

    update

--- tests/web-app.spec.js
+++ tests/web-app.spec.js
@@ -1,6 +1,16 @@
-// @ts-check
-const { test } = require("./my-setup.js")
+const { test, expect } = require("@playwright/test")
+const { CHECKLY_USER, CHECKLY_PW } = process.env
 
-test("Loads dashboard", async ({ webApp }) => {
-  await webApp.screenshot({ path: "web-app.jpg" })
+test("Loads dashboard", async ({ page }) => {
+  await page.goto("https://www.checklyhq.com/")
+  await page.getByRole("link", { name: "Log in" }).click()
+  await page.getByPlaceholder("yours@example.com").click()
+  await page.getByPlaceholder("yours@example.com").fill(CHECKLY_USER)
+  await page.getByPlaceholder("yours@example.com").press("Tab")
+  await page.getByPlaceholder("your password").fill(CHECKLY_PW)
+  await page.getByPlaceholder("your password").press("Enter")
+  await page.waitForURL("https://app.checklyhq.com/")
+
+  await expect(page.getByTestId("home-dashboard-table")).toBeVisible()
+  await page.screenshot({ path: "web-app.jpg" })
 })
